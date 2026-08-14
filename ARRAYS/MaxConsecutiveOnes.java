035cf37539ed98f8ce628d3b143e2babbc53c202
Devashish Kubade <devashishkubade2021@gmail.com>
2026-08-14 20:41:28 +0530

    Add MaxConsecutiveOnes solution 485

---
+++ ARRAYS/MaxConsecutiveOnes.java
@@ -0,0 +1,31 @@
+
+class MaxConsecutiveOness {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        MaxConsecutiveOness mco = new MaxConsecutiveOness();
+
+        int ans = mco.findMaxConsecutiveOnes(nums);
+
+        System.out.println("The maximum consecutive 1's are " + ans);
+    }
+}
